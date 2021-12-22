95c30ee9430e71f07701da0717e3ddadd8990ace
@bany <53461381+9bany@users.noreply.github.com>
2021-12-22 15:27:38 +0700

    Develop (#2)
    * checkout  pull request
    
    * fix test case on develop
    
    * update ci for development
    
    * setup express lib

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
 
-function sum(a,b) { return a + b; }
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-module.exports = sum;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
